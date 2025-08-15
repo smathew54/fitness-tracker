3a17d5268b74ec332513dd9b74ad095a5bbbef89
smathew54 <88407507+smathew54@users.noreply.github.com>
2025-08-14 20:49:52 -0500

    updating the activities page to query the API and then map out the items on to the page

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import useQuery from "../api/useQuery";
+import useMutation from "../api/useMutation";
+import { useAuth } from "../auth/AuthContext";
+
 export default function ActivitiesPage() {
+  //getting the token
+  const { token } = useAuth();
+
+  //call the function
+  const { data: activity, error, loading } = useQuery("/activities");
+
   return (
     <>
       <h1>Activities</h1>
       <p>Imagine all the activities!</p>
+
+      <div>
+        {activity?.map((item) => (
+          <div key={item.id}>
+            <h4>{item.name}</h4>
+            <p>{item.description}</p>
+            {token && <button onClick={() =>(console.log(`${item.description}`))}>Delete</button>}
+            <br></br>
+            <br></br>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
